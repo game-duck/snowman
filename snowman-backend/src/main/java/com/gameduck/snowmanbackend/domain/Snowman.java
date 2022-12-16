fade5a7573a44eac8d2514f3eb55bca3740fb706
zzambbang <zzambbang@gmail.com>
2022-12-17 00:06:48 +0900

    feat: Spring Security 로그인 회원가입 적용

--- snowman-backend/src/main/java/com/gameduck/snowmanbackend/domain/Snowman.java
+++ snowman-backend/src/main/java/com/gameduck/snowmanbackend/domain/Snowman.java
@@ -1,45 +1,36 @@
 package com.gameduck.snowmanbackend.domain;
 
-import static javax.persistence.CascadeType.*;
-
-import java.util.ArrayList;
-import java.util.List;
-
 import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
-@Getter @Setter
+@Getter
+@Setter
 @NoArgsConstructor
 public class Snowman {
 
 	@Id
 	@GeneratedValue
+	@Column(name = "snowman_id")
 	private Long id;
 
 	@ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
 	@JoinColumn(name = "user_id")
 	private User user;
 
-	private String hat;
+	private String snowmanType;
 
-	private String face;
+	private String author;
 
-	private String clothes;
-
-	private String glove;
-
-	@OneToMany(mappedBy = "post", cascade = ALL)
-	private List<Post> postList = new ArrayList<>();
-
+	private String post;
 }
