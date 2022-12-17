4bbbce938b983524ca37b88d3ce237c7e9eb1164
zzambbang <zzambbang@gmail.com>
2022-12-17 21:11:25 +0900

    fix: 백엔드 실행 오류

--- snowman_frontend/src/config.js
+++
@@ -1,8 +0,0 @@
-const BASE_URL = 'http://localhost:8000'
-export const API = {
-  LOGIN: `${BASE_URL}/user/signin`,
-  REGISTER: `${BASE_URL}`,
-  LETTER: `${BASE_URL}/faq`,
-  GARDEN: `${BASE_URL}/recruit`,
-  DESIGN: `${BASE_URL}`,
-};
