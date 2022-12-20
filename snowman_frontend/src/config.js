3e5445793bfe69c55f12a055adb55b348d6a8fb2
zzambbang <zzambbang@gmail.com>
2022-12-20 12:46:48 +0900

    fix: 깃 이그노어 파일 적용 오류

---
+++ snowman_frontend/src/config.js
@@ -0,0 +1,9 @@
+const BASE_URL = 'http://localhost:8080';
+
+export const API = {
+  REGISTER: `${BASE_URL}/api/auth/signup`,
+  LOGIN: `${BASE_URL}/api/auth/login`,
+  LETTER: `${BASE_URL}/faq`,
+  GARDEN: `${BASE_URL}/recruit`,
+  DESIGN: `${BASE_URL}`,
+};
