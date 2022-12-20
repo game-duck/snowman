d0984f497dad0f79caa21449abf58d741059a91b
zzambbang <zzambbang@gmail.com>
2022-12-20 12:45:03 +0900

    fix: 깃 이그노어 적용 오류 해결

---
+++ snowman_frontend/src/App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import styled from 'styled-components';
+import Snowfall from 'react-snowfall';
+import './App.css';
+
+import SnowmanGarden from './pages/SnowmanGarden/SnowmanGarden';
+import SnowmanDesign from './pages/SnowmanDesign/SnowmanDesign';
+import Register from './pages/User/Register';
+import Login from './pages/User/Login';
+import Letter from './pages/Letter/Letter';
+
+function App() {
+  return (
+    <div id="App">
+      <Print id="print">
+        <Snowfall />
+        <Routes>
+          <Route path="/" element={<SnowmanGarden />} />
+          <Route path="/snowmanDesign" element={<SnowmanDesign />} />
+          <Route path="/signup" element={<Register />} />
+          <Route path="/Login" element={<Login />} />
+          <Route path="/letter" element={<Letter />} />
+        </Routes>
+      </Print>
+    </div>
+  );
+}
+
+export default App;
+
+const Print = styled.div`
+  background-color: #0f1322;
+`;
