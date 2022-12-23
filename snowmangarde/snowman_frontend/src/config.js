69ea390365bdaa42a9105f19ae49430530476f80
lzns960 <lzns960@gmail.com>
2022-12-23 23:57:26 +0900

    docs: repo 통합

--- snowmangarde/snowman_frontend/src/config.js
+++
@@ -1,9 +0,0 @@
-const BASE_URL = 'http://localhost:8080';
-
-export const API = {
-  REGISTER: `${BASE_URL}/api/auth/signup`,
-  LOGIN: `${BASE_URL}/api/auth/login`,
-  SNOWMAN: `${BASE_URL}/api/snowmans/`,
-  USER: `${BASE_URL}/api/users/me`,
-  SNOWMANPOST: `${BASE_URL}/api/snowmans`,
-};
