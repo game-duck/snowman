4f036bf200a7a3430da5ada0dacccc2a5792e1a1
onezeun <onezeun@gmail.com>
2022-12-25 14:24:49 +0900

    fix: 배포관련오류수정

--- snowman-frontend/src/config.js
+++ snowman-frontend/src/config.js
@@ -1,5 +1,5 @@
-// const BASE_URL = 'https://web-snowman-20z52flc2u4w7q.gksl2.cloudtype.app/';
-const BASE_URL = process.env.REACT_APP_SNOWMAN_API_URL;
+const BASE_URL = 'https://cors-anywhere.herokuapp.com/corsdemo/https://web-snowman-20z52flc2u4w7q.gksl2.cloudtype.app/';
+// const BASE_URL = process.env.REACT_APP_SNOWMAN_API_URL;
 
 export const API = {
   REGISTER: `${BASE_URL}/api/auth/signup`,
